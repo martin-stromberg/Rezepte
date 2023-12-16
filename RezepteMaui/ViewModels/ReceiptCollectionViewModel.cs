581bbb0a623b9176afe845325c4894659b839eca
Muesli84 <m.stromberg@web.de>
2023-12-16 08:59:16 +0100

    Es können nun Rezepte direkt in Sammlungen hinzugefügt werden.

--- RezepteMaui/ViewModels/ReceiptCollectionViewModel.cs
+++ RezepteMaui/ViewModels/ReceiptCollectionViewModel.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,115 @@ namespace Rezepte.ViewModels
         public ReceiptCollectionViewModel(ReceiptLibrary receiptLibrary, INavigationManager navigationManager, IPictureStorage pictureStorage) : base(navigationManager, pictureStorage)
         {
             this.receiptLibrary = receiptLibrary;
+            MenuAction = new Command((args) => ExecuteMenuAction(args as string));
+            IsGeneralMode = true;
+        }
+
+        private void ExecuteMenuAction(string arg)
+        {
+            switch (arg)
+            {
+                case "addReceipt":
+                    ExecuteAddReceipt();
+                    break;
+                case "addURI":
+                    ExecuteAddURI();
+                    break;
+            }
+        }
+
+        private async void ExecuteAddURI()
+        {
+            IsAddingMode = false;
+            try
+            {
+                var uris = NewReceiptUri.Split("\r");
+                foreach (var uri in uris)
+                    try
+                    {
+                        var receiptUri = uri.Trim();
+                        if (string.IsNullOrWhiteSpace(receiptUri))
+                            continue;
+                        var existingReceipt = receiptLibrary.FindReceiptByUri(receiptUri);
+                        if (existingReceipt != null)
+                        {
+                            if (!receiptLibrary.IsInCollection(existingReceipt, Item))
+                            {
+                                receiptLibrary.AddToCollection(existingReceipt, Item);
+                                Add(existingReceipt);
+                            }
+                            continue;
+                        }
+                            var receipts = await receiptLibrary.CreateReceipts(receiptUri);
+                            if (receipts == null)
+                                throw new ApplicationException($"Für die angegebene Adresse konnte kein Rezept ermittelt werden.");
+                        foreach (var receipt in receipts)
+                        {
+                            existingReceipt = receiptLibrary.FindReceiptByUri(receipt.Uri);
+                            if (existingReceipt != null)
+                            {
+                                if (!receiptLibrary.IsInCollection(existingReceipt, Item))
+                                {
+                                    receiptLibrary.AddToCollection(existingReceipt, Item);
+                                    Add(existingReceipt);
+                                }                                
+                                continue;
+                            }
+
+                            receiptLibrary.Add(receipt);
+                            receiptLibrary.AddToCollection(existingReceipt, Item);
+                            Add(receipt);
+                        }
+                        
+                    }
+                    catch { }
+                IsGeneralMode = true;
+            }
+            catch
+            {
+                IsAddingMode = true;
+            }
+        }
+        public string NewReceiptUri
+        {
+            get
+            {
+                return GetProperty<string>();
+            }
+            set
+            {
+                SetProperty<string>(value);
+            }
+        }
+        private void ExecuteAddReceipt()
+        {
+            IsAddingMode = true;
+        }
+        public bool IsAddingMode
+        {
+            get
+            {
+                return GetProperty<bool>();
+            }
+            set
+            {
+                SetProperty<bool>(value);
+                if (value)
+                    IsGeneralMode = false;
+            }
+        }
+        public bool IsGeneralMode
+        {
+            get
+            {
+                return GetProperty<bool>();
+            }
+            set
+            {
+                SetProperty<bool>(value);
+                if (value)
+                    IsAddingMode = false;
+            }
         }
 
         public void SetParent(ReceiptCollection collection)
@@ -41,6 +151,8 @@ namespace Rezepte.ViewModels
 
         private long loadItemsSession = 0;
         private readonly ReceiptLibrary receiptLibrary;
+
+        public Command MenuAction { get; }
 
         private void LoadItems()
         {
